555d9c8a6fd9d85e9236dc22cb7bfd1f2148c798
caren1 <czarnocki.w@wp.pl>
2021-11-11 15:03:37 +0100

    using props in setup function

---
+++ dojo-blog-compositionapi/src/components/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post-list" >
+      <div v-for="post in posts" :key="post.id">
+          <SinglePost :post="post" />
+      </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue'
+export default {
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        // by providing props to setup function we can use them
+        // console.log(props.posts);
+    }
+}
+</script>
+
+<style>
+
+</style>
--- dojo-blog-compositionapi/src/views/Home.vue
+++ dojo-blog-compositionapi/src/views/Home.vue
@@ -1,128 +1,24 @@
 <template>
-  <div class="home">
-    <!-- home
-    <p ref="p">My name is {{ name }} and {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">increase age</button>
-    <input type="text" v-model="name" name="name" id="name"> -->
-
-    <!-- <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <hr />
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button> -->
-
-    <h1>Home</h1>
-    <input type="text" name="search" id="search" v-model="search">
-    <p>Search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      <p>{{ name }}</p>
+    <div class="home">
+        <h1>Home</h1>
+        <PostList :posts="posts"/>
     </div>
-    <button @click="handleClick">stop watching</button>
-  </div>
 </template>
 
 <script>
-import { ref, reactive, computed } from '@vue/reactivity'
-import { watch, watchEffect } from '@vue/runtime-core'
-// @ is an alias to /src
-
+import PostList from '../components/PostList.vue'
+import { ref } from '@vue/reactivity'
 export default {
-  name: 'Home',
-  // composition API setup function
-  // similar to data / mounted / lifecycle methods
-  // this function will run before any lifecycle hook
-  setup() {
-    // console.log('setup');
-    // we can write any kind of js within this function
-    // 'this' is not available in setup function = undefined
-    // variables are not reactive by default, that is why we need a way to modify them -> refs
-    
-    // const name = ref('wojt')
-    // const age = ref(27);
-    // when we use ref and surround the value it becomes reactive
+    name: 'Home',
+    components: { PostList },
+    setup() {
+        // how we can use props in setup function?
+        const posts = ref([
+            { title: 'Welcome to the blog', body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur', id: 1 },
+            { title: 'Top 5CSS tips', body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur', id: 2 }
+        ])
 
-    // const handleClick = () => {
-    //   console.log('you clicked me');
-    //   name.value = 'Mario'
-    //   age.value = 30
-      // console.log(p, p.value);
-      // p.value.classList.add('test')
-      // p.value.textContent = 'hello from ref'
-      
-      // const ninjaOne = ref({ name: 'Mario', age: 30 })
-      // const ninjaTwo = reactive({ name: 'Luigi', age: 35 })
-
-      // const updateNinjaOne = () => {
-      //   ninjaOne.value.age = 40;
-      // }
-
-      // const updateNinjaTwo = () => {
-      //   // while using reactive we do not have to use the 'value' keyword, we can directly access the value we want
-      //   ninjaTwo.age = 45
-      // }
-
-      // refs vs reactive?
-      // with reactive we cannot use primitive values
-      // const nameOne = ref('mario')
-      // const nameTwo = reactive('luigi');
-      // nameTwo = 'mario' / nameTwo = reactive('mario') <- not working
-
-      // refs will work better for external compositions
-
-      // return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
-
-      // imported from vue
-      // idea of computed is not to return a single value, but compute value on other values
-      // ie. filtered versions of data
-      // const name = computed(() => {
-      //   return 'wojt'
-      // })
-
-      // return { name }
-      const search = ref('')
-      const names = ref(['wojt', 'wojtek', 'mario', 'luigi', 'koopa'])
-
-      // we could set up a watch function - allows to run some code when some value changes
-      const stopWatch = watch(search, () => {
-        console.log('watch function ran');
-      })
-
-    // we only pass a function
-      const stopEffect = watchEffect(() => {
-        // this runs initially when component first runs
-        // it knows when to run, because we use search.value within it
-        // looks for any dependencies from inside
-        // similar to watch, but we do not have to specify one value
-        console.log('watch effect function run', search.value);
-      })
-
-      const matchingNames = computed(() => {
-        return names.value.filter((name) => name.includes(search.value))
-      })
-
-      const handleClick = () => {
-        // when we want to stop the 'watches' we assign them to variables and then invoke
-        stopWatch();
-        stopEffect()
-      }
-
-      return { names, search, matchingNames, handleClick }
-    }
-
-    // const p = ref(null);
-
-    // at the end we return values that we want to make available in template
-      // return { name, age, handleClick }
-  // },
-  // created() {
-  //   console.log('created');
-  // },
-  // mounted() {
-  //   console.log('mounted');
-  // }
+        return { posts }
+    },
 }
 </script>
