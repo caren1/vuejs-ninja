9e82fb60cf0d36c2c966a78b6089c25a5dc15fa5
caren1 <czarnocki.w@wp.pl>
2021-11-11 14:20:56 +0100

    computed values

--- dojo-blog-compositionapi/src/views/Home.vue
+++ dojo-blog-compositionapi/src/views/Home.vue
@@ -6,20 +6,26 @@
     <button @click="age++">increase age</button>
     <input type="text" v-model="name" name="name" id="name"> -->
 
-    <h1>Home</h1>
+    <!-- <h1>Home</h1>
     <h2>Refs</h2>
     <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
     <button @click="updateNinjaOne">Update ninja one</button>
     <hr />
     <h2>Reactive</h2>
     <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <button @click="updateNinjaTwo">Update ninja two</button> -->
 
+    <h1>Home</h1>
+    <input type="text" name="search" id="search" v-model="search">
+    <p>Search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from '@vue/reactivity'
+import { ref, reactive, computed } from '@vue/reactivity'
 // @ is an alias to /src
 
 export default {
@@ -45,17 +51,17 @@ export default {
       // p.value.classList.add('test')
       // p.value.textContent = 'hello from ref'
       
-      const ninjaOne = ref({ name: 'Mario', age: 30 })
-      const ninjaTwo = reactive({ name: 'Luigi', age: 35 })
+      // const ninjaOne = ref({ name: 'Mario', age: 30 })
+      // const ninjaTwo = reactive({ name: 'Luigi', age: 35 })
 
-      const updateNinjaOne = () => {
-        ninjaOne.value.age = 40;
-      }
+      // const updateNinjaOne = () => {
+      //   ninjaOne.value.age = 40;
+      // }
 
-      const updateNinjaTwo = () => {
-        // while using reactive we do not have to use the 'value' keyword, we can directly access the value we want
-        ninjaTwo.age = 45
-      }
+      // const updateNinjaTwo = () => {
+      //   // while using reactive we do not have to use the 'value' keyword, we can directly access the value we want
+      //   ninjaTwo.age = 45
+      // }
 
       // refs vs reactive?
       // with reactive we cannot use primitive values
@@ -65,7 +71,24 @@ export default {
 
       // refs will work better for external compositions
 
-      return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
+      // return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
+
+      // imported from vue
+      // idea of computed is not to return a single value, but compute value on other values
+      // ie. filtered versions of data
+      // const name = computed(() => {
+      //   return 'wojt'
+      // })
+
+      // return { name }
+      const search = ref('')
+      const names = ref(['wojt', 'wojtek', 'mario', 'luigi', 'koopa'])
+
+      const matchingNames = computed(() => {
+        return names.value.filter((name) => name.includes(search.value))
+      })
+
+      return { names, search, matchingNames }
     }
 
     // const p = ref(null);
