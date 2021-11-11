d720b275a3f77c50af9d0152c425d4fb8e10252c
caren1 <czarnocki.w@wp.pl>
2021-11-11 15:10:25 +0100

    lifecycle hooks

--- dojo-blog-compositionapi/src/components/PostList.vue
+++ dojo-blog-compositionapi/src/components/PostList.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
 import SinglePost from './SinglePost.vue'
 export default {
     props: ['posts'],
@@ -14,7 +15,26 @@ export default {
     setup(props) {
         // by providing props to setup function we can use them
         // console.log(props.posts);
-    }
+
+        // we can also use lifecycle hooks within the setup function
+        // however we need to import them from vue - with slight name change
+        // 'on' before hook name -> ie. onMounted
+
+        onMounted(() => {
+            // will fire something when component is mounted
+            console.log('mounted');
+        })
+        onUnmounted(() => {
+            console.log('unmounted');
+        })
+        onUpdated(() => {
+            console.log('updated');
+        })
+    },
+    // mounted(){
+    //     console.log('mounted with options api');
+    // }
+    // [...] we can also use them here, within options api
 }
 </script>
 
--- dojo-blog-compositionapi/src/views/Home.vue
+++ dojo-blog-compositionapi/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="home">
         <h1>Home</h1>
-        <PostList :posts="posts"/>
+        <PostList :posts="posts" v-if="showPosts"/>
+        <button @click="showPosts = !showPosts">toggle posts</button>
+        <button @click="posts.pop()">remove post</button>
+    
     </div>
 </template>
 
@@ -17,8 +20,9 @@ export default {
             { title: 'Welcome to the blog', body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur', id: 1 },
             { title: 'Top 5CSS tips', body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur', id: 2 }
         ])
+        const showPosts = ref(true);
 
-        return { posts }
+        return { posts, showPosts }
     },
 }
 </script>
