a55c6ccc068b51969fc4e1c4ef1055b0d16a8d94
caren1 <czarnocki.w@wp.pl>
2021-11-14 13:36:42 +0100

    initial commit for project planner

---
+++ ninja-planner/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+
+body {
+  background: lightgray;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+}
+</style>
