026b29abec2ac769727be7015d45ad2f4e542e78
caren1 <czarnocki.w@wp.pl>
2021-11-14 14:02:14 +0100

    using material icons

--- ninja-planner/src/components/SingleProject.vue
+++ ninja-planner/src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
       <div class="actions" @click="showDetails = !showDetails">
           <h3>{{ project.title }}</h3>
+          <div class="icons">
+              <span class="material-icons">edit</span>
+              <span class="material-icons">delete</span>
+              <span class="material-icons">done</span>
+          </div>
       </div>
       <div class="details" v-if="showDetails">
           <p>{{ project.details }}</p>
@@ -32,5 +37,24 @@ export default {
 
     h3 {
         cursor: pointer;
+    }
+
+    .actions {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .material-icons {
+        font-size: 24px;
+        margin-left: 10px;
+        color: #bbb;
+        cursor: pointer;
+    }
+    
+    .material-icons:hover {
+        color: #777;
+        transform: scale(1.1);
+        transition: all 0.1s ease-in;
     }
 </style>
