1abdf65adcdfb83488cf9897b3b727d1ade9022b
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-28 17:46:59 +0100

    navbar component

---
+++ muso-ninjas/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="navbar">
+      <nav>
+          <img src="@/assets/ninja.svg" alt="ninja-logo">
+          <h1><router-link :to="{ name: 'Home'}">Muso ninjas</router-link></h1>
+          <div class="links">
+              <button>Logout</button>
+              <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+              <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+          </div>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+     .navbar {
+    padding: 16px 10px;
+    margin-bottom: 60px;
+    background: white;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  nav img {
+    height: 50px;
+    max-height: 80px;
+  }
+  nav h1 {
+    margin-left: 20px;
+  }
+  nav .links {
+    margin-left: auto;
+  }
+  nav .links a, button {
+    margin-left: 16px;
+    font-size: 14px;
+  }
+</style>
