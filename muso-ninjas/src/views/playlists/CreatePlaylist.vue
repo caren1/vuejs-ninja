f3469f1e2d21b4c5a84567c8eabf0c389786ced2
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-28 18:06:21 +0100

    new playlist form component

---
+++ muso-ninjas/src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <h4>Create new playlist</h4>
+      <input type="text" name="title" id="title" placeholder="Playlist title" required v-model="title">
+      <textarea type="text" name="description" id="description" placeholder="Playlist description" required v-model="description"></textarea>
+      <label>Upload playlist cover image</label>
+      <input type="file">
+      <div class="error"></div>
+      <button>Create</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup() {
+        const title = ref('')
+        const description = ref('')
+
+        const handleSubmit = () => {
+            console.log(title.value, description.value);
+        }
+
+        return { title, description, handleSubmit }
+    }
+}
+</script>
+
+<style>
+  form {
+    background: white;
+  }
+  input[type="file"] {
+    border: 0;
+    padding: 0;
+  }
+  label {
+    font-size: 12px;
+    display: block;
+    margin-top: 30px;
+  }
+  button {
+    margin-top: 20px;
+  }
+</style>
