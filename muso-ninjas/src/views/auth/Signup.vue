779ba96756c615f611bc024b5c69d01c7943dc32
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-28 17:37:15 +0100

    signup view

--- muso-ninjas/src/views/auth/Signup.vue
+++ muso-ninjas/src/views/auth/Signup.vue
@@ -1,30 +1,34 @@
 <template>
   <form @submit.prevent="handleSubmit">
-      <h3>Login</h3>
+      <h3>Sign up</h3>
+      <input type="text" name="displayName" id="displayName" v-model="displayName" placeholder="Display name">
       <input type="email" name="email" id="email" v-model="email" placeholder="Email">
       <input type="password" name="password" id="password" v-model="password" placeholder="password">
       <div class="error" v-if="error">{{ error }}</div>
-      <button v-if="!isPending">log in</button>
+      <button v-if="!isPending">Sign up</button>
       <button v-else disabled>Loading..</button>
   </form>
 </template>
 
 <script>
-import useLogin from '../../composables/useLogin'
+import useSignup from '@/composables/useSignup'
 import { ref } from '@vue/reactivity'
+
 export default {
     setup() {
-        const { error, login, isPending } = useLogin();
+        const { error, signup, isPending } = useSignup();
+
+        const displayName = ref('')
         const email = ref('')
         const password = ref('')
 
         const handleSubmit = async () => {
-            const response = await login(email.value, password.value);
+            const response = await signup(email.value, password.value, displayName.value);
             if (!error.value) {
-                console.log('user logged in');
+                console.log('user registered');
             }
         }
-        return { email, password, handleSubmit, isPending, error }
+        return { displayName, email, password, handleSubmit, isPending, error }
     }
 }
 </script>
