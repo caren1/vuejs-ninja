0484c0355cb9a6dbc7bb855efe54f80b9417492a
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-28 19:59:45 +0100

    getting real time playlists data

--- muso-ninjas/src/views/Home.vue
+++ muso-ninjas/src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
     <p>Homepage</p>
+    <div class="error" v-if="error">{{ error }}</div>
+    <div v-if="documents">
+      <div class="playlists" v-for="playlist in documents" :key="playlist.id">
+        <p>{{ playlist.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection'
 export default {
   name: 'Home',
+  setup() {
+    const { documents, error } = getCollection('playlists');
+    return { documents, error }
+  }
 }
 </script>
