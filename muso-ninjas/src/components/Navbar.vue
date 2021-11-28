49814ff07955fc9b0002586216634b81148c5404
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-28 20:17:05 +0100

    nav update + route guards

--- muso-ninjas/src/components/Navbar.vue
+++ muso-ninjas/src/components/Navbar.vue
@@ -5,6 +5,7 @@
           <h1><router-link :to="{ name: 'Home'}">Muso ninjas</router-link></h1>
           <div class="links">
             <div v-if="user">
+              <router-link :to="{ name: 'CreatePlaylist' }">Create playlist</router-link>
               <button @click="handleLogout" >Logout</button>
             </div>
             <div v-else>
