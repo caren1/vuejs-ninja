c041dbaf77f0b58a4b8efb3a7f4cd157170440df
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-29 19:37:33 +0100

    listing user playlists

--- muso-ninjas/src/views/playlists/UserPlaylists.vue
+++ muso-ninjas/src/views/playlists/UserPlaylists.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="user-playlists">
-      <h2>My playlists</h2>
-
+    <h2>My playlists</h2>
+    <div v-if="playlists">
+        <ListView :playlists="playlists"/>
+    </div>
+    <router-link :to="{ name: 'CreatePlaylist' }" class="btn">Create a new playlist</router-link>
   </div>
 </template>
 
 <script>
 import getCollection from '@/composables/getCollection'
 import getUser from '@/composables/getUser'
-
+import ListView from '@/components/ListView'
 export default {
+    components: { ListView },
     setup() {
         const { user } = getUser();
         const { documents: playlists } = getCollection('playlists',
