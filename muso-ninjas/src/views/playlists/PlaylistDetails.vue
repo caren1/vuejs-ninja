a4e828b70c5bf3009382f0449b2f63215f62e92b
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-29 19:21:48 +0100

    deleting songs

--- muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -18,7 +18,7 @@
           <h3>{{ song.title }}</h3>
           <p>{{ song.artist }}</p>
         </div>
-        <button v-if="ownership">delete</button>
+        <button v-if="ownership" @click="handleClick(song.id)">delete</button>
       </div>
       <AddSong v-if="ownership" :playlist="playlist"/>
     </div>
@@ -41,7 +41,7 @@ export default {
     setup(props) {
       const { error, document: playlist } = getDocument('playlists', props.id)
       const { user } = getUser();
-      const { deleteDoc } = useDocument('playlists', props.id);
+      const { deleteDoc, updateDoc } = useDocument('playlists', props.id);
       const { deleteImage } = useStorage();
       const router = useRouter();
 
@@ -55,7 +55,14 @@ export default {
         router.push({ name: 'Home' })
       }
 
-      return { error, playlist, ownership, handleDelete }
+      const handleClick = async (songId) => {
+        const filteredSongs = playlist.value.songs.filter((song) => song.id != songId);
+        await updateDoc({
+          songs: filteredSongs
+        })
+      }
+
+      return { error, playlist, ownership, handleDelete, handleClick }
     }
 }
 </script>
