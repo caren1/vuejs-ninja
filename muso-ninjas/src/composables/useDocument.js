8ed0abf0c83e0f0fbb68d5c1913946fdfc0ea4b7
caren1 <30754368+caren1@users.noreply.github.com>
2021-11-29 19:11:38 +0100

    updating playlist resource

--- muso-ninjas/src/composables/useDocument.js
+++ muso-ninjas/src/composables/useDocument.js
@@ -20,7 +20,21 @@ const useDocument = (collection, id) => {
         }
     }
 
-    return { error, isPending, deleteDoc }
+    const updateDoc = async (updates) => {
+        isPending.value = true
+        error.value = null
+        try {
+            const response = await docRef.update(updates);
+            isPending.value = false
+            return response;
+        } catch (err) {
+            console.log(err.message);
+            isPending.value = false
+            error.value = 'Couldnt update the document'
+        }
+    }
+
+    return { error, isPending, deleteDoc, updateDoc }
 }
 
 export default useDocument
